3f36f53ad87933a55580b8a2908f8694c9052ada
Pratik <nagarepratik001@gmail.com>
2026-01-07 12:01:30 +0530

    Part6 Done

--- Part6/app.py
+++ Part6/app.py
@@ -9,7 +9,7 @@ How to Run:
 3. Open browser: http://localhost:5000
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect,url_for
 
 app = Flask(__name__)
 
@@ -21,6 +21,46 @@ TASKS = [
 ]
 
 # Your code here...
+
+@app.route("/")
+def home():
+     return render_template("index.html",tasks= TASKS)
+
+@app.route("/add",methods=["GET","POST"])
+
+def add():
+     if request.method =="POST":
+          title = request.form["title"]
+          status = request.form["status"]
+          priority = request.form["priority"]
+
+
+          new_task = {
+               "id" : len(TASKS) + 1,
+               "title": title,
+               "status":status,
+               "priority": priority
+          }     
+          TASKS.append(new_task)
+          return redirect(url_for("home"))
+     return render_template("add.html")
+
+@app.route("/task/<int:id>")
+
+def task(id):
+     for t in TASKS:
+          if t["id"] == id:
+               return render_template("task.html", task=t)
+     return "Task Does Not Found" , 404     
+
+
+
+@app.route("/about")
+
+def about():
+     return render_template("about.html")
+
+  
 
 
 if __name__ == '__main__':
